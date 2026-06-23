aef2682b9aae9611d0dcfa3f3235d10fef62391d
Deabmaister68 <514109@vistacollege.nl>
2026-06-23 12:16:29 +0200

    uh uhm oeps

---
+++ Villas Nexus/models/TimeTableRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Villas_Nexus.Models;
+
+namespace Villas_Nexus.Models
+{
+        public class TimetableRepository
+        {
+            private DatabaseConnection databaseConnection;
+
+            public TimetableRepository()
+            {
+                databaseConnection = new DatabaseConnection();
+            }
+
+            public List<Timetable> HaalAlleOptredensOp()
+            {
+                
+
+            }
+
+            public void VoegOptredenToe(Timetable timetable)
+            {
+       
+            }
+
+            public bool VerwijderOptreden(int id)
+            {
+
+            }
+        }
+    
+}
