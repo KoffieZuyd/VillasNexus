a55c5a201b11e86737ecf133112839f44bcae991
Deabmaister68 <514109@vistacollege.nl>
2026-06-23 12:44:47 +0200

    DB connecter + Time table start

---
+++ Villas Nexus/models/DBConnecter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Villas_Nexus.Models;
+
+
+namespace Villas_Nexus.Models
+{
+    public class DatabaseConnection
+    {
+        private string connectionString = "server=localhost;user=root;password=;database=vallis_nexus;";
+
+        public DatabaseConnection MaakVerbinding()
+        {
+            return new DatabaseConnection();
+        }
+    }
+    
+}
