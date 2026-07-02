ea7b34c062e026a9d9ed8e7ca5b759546a9b6f49
mohammedzarhbouch <m.zarhbouch@ponthus.nl>
2026-07-02 10:52:06 +0200

    database query's enzo

---
+++ Villas Nexus/DataAccess/ArtiestRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Villas_Nexus.Models;
+
+namespace Villas_Nexus.DataAcces
+{
+    public class ArtiestRepository
+    {
+        public List<Artiest> HaalAlleArtiestenOp()
+        {
+            List<Artiest> artiesten = new List<Artiest>();
+
+            try
+            {
+                DatabaseConnection.MaakVerbinding();
+
+                string query = "SELECT * FROM Artiest";
+
+                using (SqlCommand command = new SqlCommand(query, DatabaseConnection.connectionString))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Artiest artiest = new Artiest(
+                            Convert.ToInt32(reader["Id"]),
+                            reader["Naam"].ToString(),
+                            reader["Beschrijving"].ToString(),
+                            new List<Genre>()
+                        );
+
+                        artiesten.Add(artiest);
+                    }
+                }
+            }
+            finally
+            {
+                DatabaseConnection.SluitVerbinding();
+            }
+
+            return artiesten;
+        }
+    }
+}
---
+++ Villas Nexus/DataAccess/GenreRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Villas_Nexus.Models;
+
+namespace Villas_Nexus.DataAcces
+{
+    public class GenreRepository
+    {
+        public List<Genre> HaalAlleGenresOp()
+        {
+            List<Genre> genres = new List<Genre>();
+
+            try
+            {
+                DatabaseConnection.MaakVerbinding();
+
+                string query = "SELECT * FROM Genre";
+
+                using (SqlCommand command = new SqlCommand(query, DatabaseConnection.connectionString))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Genre genre = new Genre(
+                            Convert.ToInt32(reader["Id"]),
+                            reader["Naam"].ToString()
+                        );
+
+                        genres.Add(genre);
+                    }
+                }
+            }
+            finally
+            {
+                DatabaseConnection.SluitVerbinding();
+            }
+
+            return genres;
+        }
+
+        public bool VoegGenreToeAanArtiest(int artiestId, int genreId)
+        {
+            try
+            {
+                DatabaseConnection.MaakVerbinding();
+
+                string query = @"
+                IF NOT EXISTS (
+                    SELECT 1 
+                    FROM ArtiestGenre 
+                    WHERE ArtiestId = @ArtiestId 
+                    AND GenreId = @GenreId
+                )
+                BEGIN
+                    INSERT INTO ArtiestGenre (ArtiestId, GenreId)
+                    VALUES (@ArtiestId, @GenreId)
+                END";
+
+                using (SqlCommand command = new SqlCommand(query, DatabaseConnection.connectionString))
+                {
+                    command.Parameters.AddWithValue("@ArtiestId", artiestId);
+                    command.Parameters.AddWithValue("@GenreId", genreId);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    return rowsAffected > 0;
+                }
+            }
+            finally
+            {
+                DatabaseConnection.SluitVerbinding();
+            }
+        }
+    }
+}
