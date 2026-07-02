5b79c727d9919a472cf0d5f9a9a2f753346b0b60
AlphaDoomXVII <514345@vistacollege.nl>
2026-07-02 10:43:03 +0200

    Timetablereposetory fix

--- Villas Nexus/models/TimeTableRepository.cs
+++ Villas Nexus/models/TimeTableRepository.cs
@@ -1,37 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Villas_Nexus.Models;
 
-namespace Villas_Nexus.Models
+namespace Villas_Nexus.DataAcces
 {
-        public class TimetableRepository
+    public class TimetableRepository
+    {
+        public List<Timetable> HaalAlleOptredensOp()
         {
-            private DBConnector DBConnector;
+            List<Timetable> timetableLijst = new List<Timetable>();
 
-            public TimetableRepository()
+            try
             {
-                DBConnector = new DBConnector();
+                DBConnector.MaakVerbinding();
+
+                string query = "SELECT * FROM timetable ORDER BY Dag, StartTijd";
+
+                using (SqlCommand command = new SqlCommand(query, DBConnector.connectionString))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Timetable timetable = new Timetable(
+                            Convert.ToInt32(reader["Id"]),
+                            reader["Artiest"].ToString(),
+                            Convert.ToDateTime(reader["Dag"]),
+                            (TimeSpan)reader["StartTijd"],
+                            (TimeSpan)reader["EindTijd"],
+                            reader["Podium"].ToString(),
+                            reader["Plaats"].ToString()
+                        );
+
+                        timetableLijst.Add(timetable);
+                    }
+                }
+            }
+            finally
+            {
+                DBConnector.SluitVerbinding();
             }
 
-            public List<Timetable> HaalAlleOptredensOp()
-            {
-                
-                return new List<Timetable>();
-            }
+            return timetableLijst;
+        }
 
-            public void VoegOptredenToe(Timetable timetable)
+        public void VoegOptredenToe(Timetable timetable)
+        {
+            try
             {
-       
-            }
+                DBConnector.MaakVerbinding();
 
-            public bool VerwijderOptreden(int id)
+                string query = @"INSERT INTO timetable 
+                                (Artiest, Dag, StartTijd, EindTijd, Podium, Plaats)
+                                VALUES 
+                                (@Artiest, @Dag, @StartTijd, @EindTijd, @Podium, @Plaats)";
+
+                using (SqlCommand command = new SqlCommand(query, DBConnector.connection String))
+                {
+                    command.Parameters.AddWithValue("@Artiest", timetable.Artiest);
+                    command.Parameters.AddWithValue("@Dag", timetable.Dag);
+                    command.Parameters.AddWithValue("@StartTijd", timetable.StartTijd);
+                    command.Parameters.AddWithValue("@EindTijd", timetable.EindTijd);
+                    command.Parameters.AddWithValue("@Podium", timetable.Podium);
+                    command.Parameters.AddWithValue("@Plaats", timetable.Plaats);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            finally
             {
-                return false;
+                DBConnector.SluitVerbinding();
             }
         }
-    
+
+        public bool VerwijderOptreden(int id)
+        {
+            try
+            {
+                DBConnector.MaakVerbinding();
+
+                string query = "DELETE FROM timetable WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(query, DBConnector.connectionString))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    return rowsAffected > 0;
+                }
+            }
+            finally
+            {
+                DBConnector.SluitVerbinding();
+            }
+        }
+    }
 }
+
+
+
